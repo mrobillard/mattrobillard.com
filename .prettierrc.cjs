747cd7bca29d6d3a23d9bf23ba8c063cdef66166
Matt Robillard <robillard.matt@gmail.com>
2024-08-15 00:29:54 -0400

    build: add turbo repo and build utils

---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+// @ts-check
+
+/** @type {import("prettier").Config} */
+module.exports = {
+    singleQuote: true,
+    quoteProps: 'consistent',
+    trailingComma: 'all',
+    plugins: [require.resolve('prettier-plugin-tailwindcss')],
+  };
+  
