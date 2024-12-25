c6125804a03bb8779f6cbaab38abb24e4ec7ef92
Matt Robillard <robillard.matt@gmail.com>
2024-12-25 09:39:20 -0500

    feat: add basic theme styles to tailwind config

--- website/tailwind.config.ts
+++ website/tailwind.config.ts
@@ -1,20 +1,22 @@
 import type { Config } from 'tailwindcss';
+import { fontFamily } from 'tailwindcss/defaultTheme';
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        background: '#F2F7F7',
+        navy: '#0B2436',
+      },
+      fontFamily: {
+        sans: ['var(--font-sans)', ...fontFamily.sans],
+        mono: ['var(--font-mono)', ...fontFamily.mono],
+        serif: ['var(--font-serif)', ...fontFamily.serif],
       },
     },
   },
   plugins: [],
 };
+
 export default config;
