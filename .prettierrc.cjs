64363e28c95a52111c8c18972fc8b09bb6684270
Matt Robillard <robillard.matt@gmail.com>
2024-12-24 22:34:15 -0500

    chore: clean up repo and add clsx and tailwind-merge dependencies

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -2,9 +2,8 @@
 
 /** @type {import("prettier").Config} */
 module.exports = {
-    singleQuote: true,
-    quoteProps: 'consistent',
-    trailingComma: 'all',
-    plugins: [require.resolve('prettier-plugin-tailwindcss')],
-  };
-  
+  singleQuote: true,
+  quoteProps: 'consistent',
+  trailingComma: 'all',
+  plugins: [require.resolve('prettier-plugin-tailwindcss')],
+};
