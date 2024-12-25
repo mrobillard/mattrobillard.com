b562e35d19de390943c688bc8569d850e10e532b
Matt Robillard <robillard.matt@gmail.com>
2024-12-24 22:56:59 -0500

    feat: add site fonts

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -1,23 +1,59 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import './globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
+import type { Metadata } from 'next';
+import localFont from 'next/font/local';
+import { Barlow, Roboto_Mono } from 'next/font/google';
+import clsx from 'clsx';
+
+const BarlowFont = Barlow({
+  variable: '--font-sans',
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+});
+
+const RobotoMonoFont = Roboto_Mono({
+  variable: '--font-mono',
+  subsets: ['latin'],
+});
+
+const TestTiemposFont = localFont({
+  variable: '--font-serif',
+  src: [
+    {
+      path: '../../public/fonts/TiemposHeadline-Light.woff2',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/TiemposHeadline-LightItalic.woff2',
+      weight: '300',
+      style: 'italic',
+    },
+  ],
+});
 
 export const metadata: Metadata = {
-    title: "Matt Robillard",
-    description : "Matt Robillard's Personal Site"
+  title: 'Matt Robillard',
+  description: 'Matt Robillard Personal Site',
 };
 
 export default function RootLayout({
-    children,
-
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="en">
-            <body className={inter.className}>{children}</body>
-        </html>
-    ):
-} 
+  return (
+    <html lang="en">
+      <body
+        className={clsx(
+          BarlowFont.className,
+          BarlowFont.variable,
+          RobotoMonoFont.variable,
+          TestTiemposFont.variable,
+        )}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
